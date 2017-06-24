2d30b511587485eee74369e48351b7fddabf6c36
RushanB <Rushanben@hotmail.com>
2017-06-24 02:30:36 -0400

    Major Layout changes - Working

--- CourseTracker/CourseTracker/ViewController.swift
+++ CourseTracker/CourseTracker/ViewController.swift
@@ -9,24 +9,66 @@
 import UIKit
 import RealmSwift
 
-class ViewController: UIViewController {
+import IBAnimatable
 
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    var student: Student!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //segue to Calendar
-        performSegue(withIdentifier: "PresentCalendar", sender: self)
         
-//        UofTAPI.updateDB()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-    
-        sleep(5)
+        //Delay
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4), execute: {
+            
+            //segue to Calendar
+            self.performSegue(withIdentifier: "PresentCalendar", sender: self)
+            
+        })
+        //        UofTAPI.updateDB()
+        
+        //Student
+        var students: [Student]
+        
+        do {
+            students = try Array(Realm().objects(Student.self))
+        }
+        catch let error {
+            print("Realm read error: \(error.localizedDescription)")
+            fatalError()
+        }
+        
+        if let _student = students.first {
+            student = _student
+        }
+        else {
+            student = Student()
+            student.name = "rush"
+            
+            do {
+                let realm = try Realm()
+                try realm.write {
+                    realm.add(student, update: true)
+                }
+            }
+            catch let error {
+                print("Realm read error: \(error.localizedDescription)")
+                fatalError()
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PresentCalendar" {
+            let calendarVC = segue.destination as! CalendarViewController
+            calendarVC.student = student
+        }
     }
     
     
