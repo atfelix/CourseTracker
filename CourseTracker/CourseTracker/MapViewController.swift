06a79cc370cfd83216e7f30e2c42bc630f1fdd66
Adam Felix <adam.tyler.felix@gmail.com>
2017-06-28 15:28:04 -0400

    Add walking directions to map

--- CourseTracker/CourseTracker/MapViewController.swift
+++ CourseTracker/CourseTracker/MapViewController.swift
@@ -28,7 +28,15 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     //print labels in middle of map
     var middlePoint: MKMapPoint!
     var time: Int = 0
-    var distance: Float = 0
+    var distance: Float = 0 {
+        didSet {
+            self.distanceLabel.alpha = 1.0
+            self.distanceLabel.text = String(format: "Distance = %.0fm", distance)
+        }
+    }
+    var currentOverlays: [MKOverlay?] = []
+    var buildingArray: [MKPointAnnotation]!
+    var width = 0
     
     //realm
     var realm: Realm!
@@ -44,18 +52,26 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         mapView.mapType = .standard
         mapView.isZoomEnabled = true //zoom
         mapView.isScrollEnabled = true //scroll
-        
+
         //Set Core Location
+
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
-        
-        let buildingArray = getBuildingInfo()
+
         //add buildings to the map
 
+        buildingArray = getBuildingInfo()
+
         mapView.addAnnotations(buildingArray)
-        mapView.selectAnnotation(buildingArray[0], animated: false)
+        self.width = buildingArray.count - 1
+
+        mapView.selectAnnotation(buildingArray[0], animated: true)
+
+        for i in stride(from: buildingArray.count - 2, through: 0, by: -1) {
+            addDirections(between: buildingArray[i + 1], and: buildingArray[i])
+        }
 
         if let maxLatitude = buildingArray.map({ $0.coordinate.latitude }).max(),
             let minLatitude = buildingArray.map({ $0.coordinate.latitude }).min(),
@@ -65,41 +81,53 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
             let centerPoint = CLLocationCoordinate2D(latitude: (minLatitude + maxLatitude) / 2, longitude: (minLongitude + maxLongitude) / 2)
             let latitudinalMeters = CLLocation(latitude: minLatitude, longitude: minLongitude).distance(from: CLLocation(latitude: maxLatitude, longitude: minLongitude))
             let longitudinalMeters = CLLocation(latitude: minLatitude, longitude: minLongitude).distance(from: CLLocation(latitude: minLatitude, longitude: maxLongitude))
-            let region = MKCoordinateRegionMakeWithDistance(centerPoint, 1.8 * latitudinalMeters, 1.8 * longitudinalMeters)
+            let region = MKCoordinateRegionMakeWithDistance(centerPoint, 3 * latitudinalMeters, 3 * longitudinalMeters)
             mapView.setRegion(region, animated: true)
         }
 
 
         
         //connect all the events using poly line
-        var points : [CLLocationCoordinate2D] = [CLLocationCoordinate2D]()
+        var points = [CLLocationCoordinate2D]()
         
-        for annotation in buildingArray{
+        for annotation in buildingArray {
             points.append(annotation.coordinate) //append the points
         }
+
+        distanceLabel.text = ""
         
-        let polyline = MKPolyline(coordinates: &points, count: points.count)
-        //add the poly line to the map
-        mapView.add(polyline)
+        //let polyline = MKPolyline(coordinates: &points, count: points.count)
+//        add the poly line to the map
+        //mapView.add(polyline)
         
     }
-    //
-    //    override func viewWillAppear(_ animated: Bool) {
-    //        super.viewWillAppear(animated)
-    //
-    //        //Set Core Location
-    //        startLocationUpdates()
-    //    }
-    
-    //MARK: Helper methods
-    
-    //    func getDirections(){
-    //        if let selectedPin = selectedPin {
-    //            let mapItem = MKMapItem(placemark: selectedPin)
-    //            let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
-    //            mapItem.openInMapsWithLaunchOptions(launchOptions)
-    //        }
-    //    }
+
+    func addDirections(between source: MKPointAnnotation, and destination: MKPointAnnotation) {
+        let sourceMapItem = MKMapItem(placemark: MKPlacemark(coordinate: source.coordinate, addressDictionary: nil))
+        let destinationMapItem = MKMapItem(placemark: MKPlacemark(coordinate: destination.coordinate, addressDictionary: nil))
+        let directionsRequest = MKDirectionsRequest()
+        directionsRequest.source = sourceMapItem
+        directionsRequest.destination = destinationMapItem
+        directionsRequest.transportType = .walking
+        let directions = MKDirections(request: directionsRequest)
+
+        directions.calculate { [weak self]
+            (response, error) in
+            guard let response = response else {
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                }
+                return
+            }
+
+            if let welf = self,
+                let route = response.routes.first {
+                welf.mapView.addOverlays([route.polyline], level: .aboveRoads)
+                print(route.polyline.boundingMapRect.origin)
+                print(sourceMapItem)
+            }
+        }
+    }
     
     //return to calendar
     @IBAction func returnButtonTapped(_ sender: Any) {
@@ -124,8 +152,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         let myLocation = CLLocation(latitude: 43.66, longitude:-79.39)
         var buildings = [MKPointAnnotation]()
         let day = DaysOfWeek(rawValue: Calendar.current.component(.weekday, from: date))!
-        
-        //MARK: this might mess shit up
+
         for course in student.coursesFor(day: day) {
             
             if let time = course.courseTimeFor(day: day).first,
@@ -136,19 +163,13 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                 let annotationView = MKPointAnnotation()
                 annotationView.coordinate.latitude = latitude
                 annotationView.coordinate.longitude = longitude
-                annotationView.title = building.name
-
-                
-//                let annotation = BuildingData(latitude: latitude, longitude: longitude)
-//                annotation.name = building.name
-//                annotation.address = building.address?.street
-//                annotationView.annotation = annotation
+                annotationView.title = course.code + ":  " + time.location
+                annotationView.subtitle = time.startTime.convertSecondsFromMidnight() + "-" + time.endTime.convertSecondsFromMidnight()
 
                 buildings.append(annotationView)
                 
                 let distance = myLocation.distance(from: CLLocation(latitude: latitude, longitude: longitude))
                 let result = String(format: "%.1f", distance / 1000)
-                self.distanceLabel.text = "Distance = \(result) KM"
             }
         }
         
@@ -183,7 +204,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                 //set the distance label to current distance
                 let distance = myLocation.distance(from: CLLocation(latitude: lat, longitude: long))
                 let result = String(format: "%.1f", distance/1000)
-                self.distanceLabel.text = "Distance = \(result) KM" //add time to get to route
+                self.distanceLabel.text = "" //add time to get to route
                 
             }
         }
@@ -196,9 +217,11 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let render = MKPolylineRenderer(overlay: overlay)
         //if there is a line make it blue and wide
-        if overlay is MKPolyline{
-            render.strokeColor = UIColor.blue
-            render.lineWidth = 1.0
+        if overlay is MKPolyline {
+            currentOverlays.append(overlay)
+            render.strokeColor = [UIColor.blue, UIColor.red, UIColor.black, UIColor.green, UIColor.brown][self.width % 5]
+            render.lineWidth = CGFloat(2 * self.width + 2)
+            self.width = max(0, width - 1)
         }
         return render
     }
@@ -228,5 +251,36 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error determining location: \(error)")
+    }
+
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+
+        mapView.removeOverlays(currentOverlays as! [MKOverlay])
+        currentOverlays.removeAll()
+
+        guard
+            let view = view as? MKPinAnnotationView,
+            let annotation = view.annotation as? MKPointAnnotation,
+            let index = buildingArray.index(of: annotation) else {
+                return
+        }
+        if index == buildingArray.count - 1 {
+            addDirections(between: buildingArray[index - 1], and: buildingArray[index])
+            let x = CLLocation(latitude: buildingArray[index - 1].coordinate.latitude, longitude: buildingArray[index - 1].coordinate.longitude)
+            let y = CLLocation(latitude: buildingArray[index].coordinate.latitude, longitude: buildingArray[index].coordinate.longitude)
+            self.distance = Float(x.distance(from: y))
+        }
+        else {
+            addDirections(between: buildingArray[index], and: buildingArray[index + 1])
+            let x = CLLocation(latitude: buildingArray[index].coordinate.latitude, longitude: buildingArray[index].coordinate.longitude)
+            let y = CLLocation(latitude: buildingArray[index + 1].coordinate.latitude, longitude: buildingArray[index + 1].coordinate.longitude)
+            self.distance = Float(x.distance(from: y))
+
+        }
+    }
+
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        mapView.removeOverlays(currentOverlays as! [MKOverlay])
+        currentOverlays.removeAll()
     }
 }
