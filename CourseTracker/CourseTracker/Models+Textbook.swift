4684fe84d2a2e080cb379c1e01b5873ebb480c96
Adam Felix <adam.tyler.felix@gmail.com>
2017-07-04 12:25:03 -0400

    Add convenience initializers for Textbook related objects
    Add convenience initializers for Textbook, TextbookCourse,
    and TextbookMeetingSection

--- CourseTracker/CourseTracker/Models+Textbook.swift
+++ CourseTracker/CourseTracker/Models+Textbook.swift
@@ -13,6 +13,23 @@ import RealmSwift
 final class TextbookMeetingSection: Object {
     dynamic var code = ""
     let instructors = List<RealmString>()
+
+    convenience init?(fromJSON json: [String:Any]) {
+        self.init()
+
+        guard
+            let code = json["code"] as? String,
+            let instructors = json["instructors"] as? [String] else {
+                print("JSON does not conform to TextbookMeetingSection Prototype JSON")
+                return nil
+        }
+
+        self.code = code
+
+        for instructor in instructors {
+            self.instructors.append(RealmString(string: instructor))
+        }
+    }
 }
 
 final class TextbookCourse: Object {
@@ -23,6 +40,23 @@ final class TextbookCourse: Object {
 
     override static func primaryKey() -> String? {
         return "id"
+    }
+
+    convenience init?(fromJSON json: [String:Any]) {
+        self.init()
+
+        guard
+            let id = json["id"] as? String,
+            let code = json["code"] as? String,
+            let requirement = json["requirement"] as? String,
+            let _ = json["meeting_sections"] as? [[String:Any]] else {
+                print("JSON does not conform to TextbookCourse Prototype JSON")
+                return nil
+        }
+
+        self.id = id
+        self.code = code
+        self.requirement = requirement
     }
 }
 
@@ -39,5 +73,32 @@ final class Textbook: Object {
 
     override static func primaryKey() -> String? {
         return "id"
+    }
+
+    convenience init?(fromJSON json: [String:Any]) {
+        self.init()
+
+        guard
+            let id = json["id"] as? String,
+            let isbn = json["isbn"] as? String,
+            let title = json["title"] as? String,
+            let edition = json["edition"] as? Int,
+            let author = json["author"] as? String,
+            let imageURL = json["image"] as? String,
+            let price = json["price"] as? Double,
+            let url = json["url"] as? String,
+            let _ = json["courses"] as? [[String:Any]] else {
+                print("JSON does not conform to Textbook Prototype JSON")
+                return
+        }
+
+        self.id = id
+        self.isbn = isbn
+        self.title = title
+        self.edition = edition
+        self.author = author
+        self.imageURL = imageURL
+        self.price = price
+        self.url = url
     }
 }
--- CourseTracker/CourseTracker/UofTAPI+Textbook.swift
+++ CourseTracker/CourseTracker/UofTAPI+Textbook.swift
@@ -41,28 +41,11 @@ extension UofTAPI {
 
     static func addOrUpdateTextbook(fromJSON json: [String:Any]) {
         guard
-            let id = json["id"] as? String,
-            let isbn = json["isbn"] as? String,
-            let title = json["title"] as? String,
-            let edition = json["edition"] as? Int,
-            let author = json["author"] as? String,
-            let imageURL = json["image"] as? String,
-            let price = json["price"] as? Double,
-            let url = json["url"] as? String,
+            let textbook = Textbook(fromJSON: json),
             let courses = json["courses"] as? [[String:Any]] else {
                 print("JSON does not conform to Textbook Prototype JSON")
                 return
         }
-
-        let textbook = Textbook()
-        textbook.id = id
-        textbook.isbn = isbn
-        textbook.title = title
-        textbook.edition = edition
-        textbook.author = author
-        textbook.imageURL = imageURL
-        textbook.price = price
-        textbook.url = url
 
         for course in courses {
             addOrUpdateTextbookCourse(textbook: textbook, fromJSON: course)
@@ -80,18 +63,11 @@ extension UofTAPI {
 
     static func addOrUpdateTextbookCourse(textbook: Textbook, fromJSON json: [String:Any]) {
         guard
-            let id = json["id"] as? String,
-            let code = json["code"] as? String,
-            let requirement = json["requirement"] as? String,
+            let textbookCourse = TextbookCourse(fromJSON: json),
             let meetingSections = json["meeting_sections"] as? [[String:Any]] else {
                 print("JSON does not conform to TextbookCourse Prototype JSON")
                 return
         }
-
-        let textbookCourse = TextbookCourse()
-        textbookCourse.id = id
-        textbookCourse.code = code
-        textbookCourse.requirement = requirement
 
         for meetingSection in meetingSections {
             addOrUpdateTextbookMeetingSection(textbookCourse: textbookCourse, fromJSON: meetingSection)
@@ -109,20 +85,9 @@ extension UofTAPI {
     }
 
     static func addOrUpdateTextbookMeetingSection(textbookCourse: TextbookCourse, fromJSON json: [String:Any]) {
-        guard
-            let code = json["code"] as? String,
-            let instructors = json["instructors"] as? [String] else {
-                print("JSON does not conform to TextbookMeetingSection Prototype JSON")
-                return
-        }
-
-        let textbookMeetingSection = TextbookMeetingSection()
-        textbookMeetingSection.code = code
-
-        for instructor in instructors {
-            let realmString = RealmString()
-            realmString.string = instructor
-            textbookMeetingSection.instructors.append(realmString)
+        guard let textbookMeetingSection = TextbookMeetingSection(fromJSON: json) else {
+            print("JSON does not conform to TextbookMeetingSection Prototype JSON")
+            return
         }
 
         do {
